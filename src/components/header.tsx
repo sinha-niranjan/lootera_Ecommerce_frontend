00d5adbf9f47bb5c01be035044bd761487640a99
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-12-30 10:55:31 +0530

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { useState } from "react";
+
+const user = { _id: "dfd", role: "admin" };
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = () => {
+    setIsOpen(false);
+  };
+  return (
+    <nav className="header">
+      <Link to={"/"} onClick={() => setIsOpen(false)}>
+        {" "}
+        Home
+      </Link>
+      <Link to={"/search"} onClick={() => setIsOpen(false)}>
+        <FaSearch />
+      </Link>
+      <Link to={"/cart"} onClick={() => setIsOpen(false)}>
+        <FaShoppingBag />
+      </Link>
+
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user?.role == "admin" && (
+                <Link to="/admin/dashboard" onClick={() => setIsOpen(false)}>
+                  {" "}
+                  Admin{" "}
+                </Link>
+              )}
+              <Link to={"/orders"} onClick={() => setIsOpen(false)}>
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          {" "}
+          <FaSignInAlt />{" "}
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
