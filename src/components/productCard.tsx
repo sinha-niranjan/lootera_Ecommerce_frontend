0e743fb3d6297e72c5919f6a2a5e48e0c5f6bfea
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-01-10 02:02:11 +0530

    Changes to be committed: 	new file:   src/assets/cover.jpg 	new file:   src/components/productCard.tsx 	modified:   src/pages/home.tsx 	modified:   src/styles/app.scss 	new file:   src/styles/home.scss

---
+++ src/components/productCard.tsx
@@ -0,0 +1,37 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProductProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+
+const server = "jdkfdfkjdf";
+
+const ProductCard = ({
+  productId,
+  price,
+  name,
+  photo,
+  stock,
+  handler,
+}: ProductProps) => {
+  return (
+    <div className="productCard">
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
+
+      <div>
+        <button onClick={() => handler()}>
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
